2cd2a5a7366ea4e748dd2faa831752191ae81841
B-Swayze <bradley>
2021-05-18 23:00:22 -0500

    tile assets added - bomb | spikes | powerup

---
+++ data/map/hazard.tsx
@@ -0,0 +1,28 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.5.0" name="Hazard" tilewidth="60" tileheight="60" tilecount="8" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="3">
+  <image width="60" height="60" source="../../../../../Documents/Brad's Stuff/Reason Sessions/Astoria Legend/Web Game/Tiles/Special Tiles/_0000_powerUp.png"/>
+ </tile>
+ <tile id="4">
+  <image width="60" height="60" source="../../../../../Documents/Brad's Stuff/Reason Sessions/Astoria Legend/Web Game/Tiles/Special Tiles/_0001_bomb_3.png"/>
+ </tile>
+ <tile id="6">
+  <image width="60" height="60" source="../../../../../Documents/Brad's Stuff/Reason Sessions/Astoria Legend/Web Game/Tiles/Special Tiles/_0003_bom_2.png"/>
+ </tile>
+ <tile id="7">
+  <image width="60" height="60" source="../../../../../Documents/Brad's Stuff/Reason Sessions/Astoria Legend/Web Game/Tiles/Special Tiles/_0004_bomb_1.png"/>
+ </tile>
+ <tile id="8">
+  <image width="60" height="60" source="../../../../../Documents/Brad's Stuff/Reason Sessions/Astoria Legend/Web Game/Tiles/Special Tiles/_0005_spikes.png"/>
+ </tile>
+ <tile id="9">
+  <image width="60" height="60" source="../../../../../Documents/Brad's Stuff/Reason Sessions/Astoria Legend/Web Game/Tiles/Special Tiles/_0006_spike_corner.png"/>
+ </tile>
+ <tile id="10">
+  <image width="60" height="60" source="../../../../../Documents/Brad's Stuff/Reason Sessions/Astoria Legend/Web Game/Tiles/Special Tiles/_0002_bomb_5.png"/>
+ </tile>
+ <tile id="11">
+  <image width="60" height="60" source="../../../../../Documents/Brad's Stuff/Reason Sessions/Astoria Legend/Web Game/Tiles/Special Tiles/_0002_bomb_6.png"/>
+ </tile>
+</tileset>
